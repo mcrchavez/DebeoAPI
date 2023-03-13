b25c166233125844e66532a303de792b7a4d45b0
moody2002 <mo.mabrouk17@gmail.com>
2023-03-12 22:48:11 -0400

    few final comments for presentation

--- src/team.js
+++ src/team.js
@@ -2,22 +2,22 @@ class Team{
     constructor(name, id) {
         this.members = new Map(); //Our collection of users
         this.name = name;      // name of the team.
-        this.id = id;
-        this.admin = null;
-        this.count = 0;
+        this.id = id;// team id
+        this.admin = null; //current team admin
+        this.count = 0; //number of members
 
     }
 
     set_admin(user){
-        this.admin = user;
+        this.admin = user;//set admin
     }
 
     get_admin(){
-        return admin
+        return admin;//return current admin
     }
 
     add_user(user){
-        if(this.count == 0){
+        if(this.count == 0){//if first member, make admin
             this.set_admin(user);
         }
         this.count += 1;
--- src/transaction.js
+++ src/transaction.js
@@ -4,16 +4,16 @@ class Transaction{
     static transaction_counter = 0;
 
     constructor(transaction_id, total, debt, date, paid_off){
-        transaction_counter = transaction_counter + 1;
+        transaction_counter = transaction_counter + 1;//increment counter
         var transaction_id = transaction_counter;
         this.total = total;
-        var debt = make2DArray(0, 0);
+        var debt = make2DArray(0, 0);//debt matrix (to be updated)
         var date = date;
         this.paid_off = paid_off;
     }
 
     get transaction_id(){
-        return this.transaction_id;
+        return this.transaction_id;//return current transaction id
     }
     get debt(){
         return this.debt;
