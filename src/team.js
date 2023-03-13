e6f16769641129c69cddcc0b60edd62421194127
mcrchavez <mcrchavez713@gmail.com>
2023-03-12 23:10:33 -0400

    Fixed toString

--- src/team.js
+++ src/team.js
@@ -2,25 +2,34 @@ class Team{
     constructor(name, id) {
         this.members = new Map(); //Our collection of users
         this.name = name;      // name of the team.
-        this.id = id;// team id
-        this.admin = null; //current team admin
-        this.count = 0; //number of members
+        this.id = id;
+        this.admin = null;
+        this.count = 0;
 
     }
     toString(){
-        return "Team Name: " + this.name + this.members.entries.toString();
+        str = ""
+        str += "Team Name: " + this.name + "\n";
+        for (let [key, value] of this.members){
+            if(key == this.admin){
+                str += "Admin: " + key + ", ID: " + value + "\n";
+            }
+            else{
+                str += "Name: " + key + ", ID: " + value + "\n";
+            }
+        }
     }
 
     set_admin(user){
-        this.admin = user;//set admin
+        this.admin = user;
     }
 
     get_admin(){
-        return admin;//return current admin
+        return admin
     }
 
     add_user(user){
-        if(this.count == 0){//if first member, make admin
+        if(this.count == 0){
             this.set_admin(user);
         }
         this.count += 1;
